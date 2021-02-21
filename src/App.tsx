6034e3fe674af9f835a6dfed05f2302d0b62ba68
Sergei-Illarionov <mestre_1000@mail.ru>
2021-02-21 19:03:41 +0300

    task 1. create components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./TodoList";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const tasksOne: Array<TaskType> = [
+        {id: 1, title: "JS", isDone: false},
+        {id: 2, title: "HTML", isDone: true},
+        {id: 3, title: "CSS", isDone: false},
+    ]
+    const tasksTwo: Array<TaskType> = [
+        {id: 1, title: "Milk", isDone: false},
+        {id: 2, title: "Bread", isDone: false},
+        {id: 3, title: "Water", isDone: true},
+    ]
+    return (
+        <div className="App">
+            <TodoList title="What to learn" tasks={tasksOne}/>
+            <TodoList title="What to buy" tasks={tasksTwo}/>
+        </div>
+    );
 }
 
 export default App;
+
